e6aad993e3ed0129c4b1c5d564dec2936279fe94
PagezLwando <lwandonodume@gmail.com>
2017-09-27 14:49:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title> LKN | Home </title>
+  </head>
+  <body>
+
+    <div class="flex-container">
+      <header>
+        <h1><!--<span><img src="images/logo.jpg"></span>--> My Information </h1>
+        <h6> Let it go. Move on. Forgive yourself. </h6>
+      </header>
+
+      <nav class="nav">
+	      <ul>
+			  <li class="active"><a href="index.php"> Home </a></li>
+			  <li><a href="pages/about.php"> About </a></li>
+			  <li><a href="pages/academics.php"> Academics </a></li>
+			  <li><a href="pages/services.php"> Services Offered </a></li>
+			  <li><a href="pages/gallery.php"> Gallery </a></li>
+			  <li><a href="pages/activities.php"> Activities </a></li>
+			  <li><a href="pages/adverts.php"> Adverts </a></li>
+		  </ul>
+      </nav>
+
+	<article class="article">
+	<div class="left">
+		<h3><span> Hi I'm <span style="color: darkred;"> Lwando Knowledge Nodume </span></h3>
+		<p> 
+			FULL STACK WEB <span style="color: brown;"> DEVELOPER </span>
+			<br>
+			JAVA DEVELOPER
+		</p>
+		<p>		</p>
+		<p> I like to develop applications that will help me like a calculator, <br>
+		and web pages.</p>
+		<p><strong>Am a nice and good person!</strong></p>
+	</div>
+		<div class="right">
+			<img style="border-radius: 10px 10px 10px 10px; float: right; width: 60%; height: 50%;" src="images/Icon.png">
+		</div>
+			
+
+	</article>
+
+	<footer>Copyright &copy; lwandonodume.com</footer>
+	</div>
+
+</body>
+</html>
